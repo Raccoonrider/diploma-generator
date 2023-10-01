4240d4bea978e52f424a7473172907d35be56878
Avermakov <avermakov@list.ru>
2023-10-01 23:34:09 +0600

    Update app

--- src/app.py
+++ src/app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 
 @app.route("/xcm_kabanova/2023/<int:user_id>/html/")
 def xcm_osenniy_2023_html(user_id):
-    print(request.base_url)
     result = Result.get(user_id)
     if result is None: abort(404)
 
@@ -18,7 +17,7 @@ def xcm_osenniy_2023_html(user_id):
 
 @app.route("/xcm_kabanova/2023/<int:user_id>/pdf/")
 def xcm_osenniy_2023_pdf(user_id):
-    html_url = request.base_url.replace("pdf", "html")
+    html_url = request.base_url.replace("pdf", "html").replace("http", "https")
     pdf = pdfkit.from_url(html_url)
 
     return Response(pdf, mimetype="application/pdf")
